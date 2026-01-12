4e95117230b3ccbbdf46002142090ca4390be5d6
Ana <anaizma16@gmail.com>
2026-01-11 21:54:48 -0500

    Chore: changing meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,15 @@ import "./globals.css";
 import type { Metadata } from "next";
 import Navbar from "@/components/Navbar";
 
-export const metadata: Metadata = {
-  title: "CurioLab",
-  description: "Build. Debug. Discover.",
+export const metadata = {
+  title: {
+    default: "CurioLab",
+    template: "%s | CurioLab",
+  },
+  description:
+    "CurioLab is a project-based STEM program where students turn ideas into real engineering builds, guided by mentors in a collaborative, hands-on environment.",
 };
+
 
 export default function RootLayout({
   children,
