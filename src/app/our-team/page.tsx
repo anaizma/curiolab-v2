1c1ef9f4d099b7a24712ace24e93c6cbec5cb64f
Ana <anaizma16@gmail.com>
2026-01-11 21:34:05 -0500

    Chore: fixing rober's picture

--- src/app/our-team/page.tsx
+++ src/app/our-team/page.tsx
@@ -78,7 +78,7 @@ const TEAM: Member[] = [
     {
     name: "Rober Carrillo",
     role: "Curio Developer",
-    photo: "/projects/rober.jpg",
+    photo: "/projects/rober.png",
     socials: {
       linkedin: "https://linkedin.com/",
       github: "https://github.com/",
